2ca26dff748508f75e77e5bf986f19178fa1855b
HieuNN <nguyen.ngoc.hieu-b@sun-asterisk.com>
2022-02-28 11:15:23 +0700

    Add Chapter 9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,13 @@
 class SessionsController < ApplicationController
+  before_action :find_user, only: :create
+
   def new; end
 
   def create
-    user = User.find_by email: params[:session][:email].downcase
-    if user&.authenticate params[:session][:password]
-      log_in user
-      redirect_to user
+    if @user&.authenticate params[:session][:password]
+      log_in @user
+      params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       flash.now[:danger] = t(".invalid_user")
       render :new
@@ -15,5 +17,10 @@ class SessionsController < ApplicationController
   def destroy
     log_out
     redirect_to root_url
+  end
+
+  private
+  def find_user
+    @user = User.find_by email: params[:session][:email].downcase
   end
 end
